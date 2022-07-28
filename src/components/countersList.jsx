b95e3ef46e11d5cdd5fd7a7cd5c77848339f25d8
PavelKn <ferrari.inside@gmail.com>
2022-07-28 15:33:27 +0300

    add onIncrement

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -3,8 +3,8 @@ import Counter from "./counter";
 
 const CountersList = () => {
   const initialState = [
-    { id: 0, value: 0, name: "Ненужная вещь", price: "200" },
-    { id: 1, value: 4, name: "Ложка" },
+    { id: 0, value: 0, name: "Ненужная вещь" },
+    { id: 1, value: 0, name: "Ложка" },
     { id: 2, value: 0, name: "Вилка" },
     { id: 3, value: 0, name: "Тарелка" },
     { id: 4, value: 0, name: "Набор минималиста" },
@@ -21,10 +21,25 @@ const CountersList = () => {
     setCounters(initialState);
   };
 
+  const handleIncrement = (id) => {
+    console.log("handle Increment");
+  };
+
+  const handleDecrement = () => {
+    console.log("handle decrement");
+    //setCounters((prevState) => prevState - 1);
+  };
+
   return (
     <>
       {counters.map((count) => (
-        <Counter key={count.id} onDelete={handleDelete} {...count} />
+        <Counter
+          key={count.id}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          {...count}
+        />
       ))}
       <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>
         Сброс
